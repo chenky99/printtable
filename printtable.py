be656f2461e4b371faa4d38ffbed00671ffe47a3
Eun Kyoung Choi <ekchoi@UG.KTH.SE>
2019-09-11 15:31:02 +0200

    added power table

--- printtable.py
+++ printtable.py
@@ -4,9 +4,12 @@ def multtable(number, limit):
 
 multtable(7, 10)
 
+import math
 
-def new(number, limit):
+def powertable(number, limit):
     for i in range(1, limit+1):
-        print(i * number)
-new(13, 30)
-
+        power = math.pow(i, number)
+        i =+ 1
+        print(power)
+powertable(2,4)
+powertable(3,3)
